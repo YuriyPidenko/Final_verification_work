29991bdd71bc3fb6f1690a1048f5d11ea5073fed
YuriyPidenko <120717893+YuriyPidenko@users.noreply.github.com>
2023-01-23 16:45:07 +0300

    The solution of the problem

---
+++ a_task/Program.cs
@@ -0,0 +1,29 @@
+﻿void MethodCreateInputArray(string[] InputArray)
+{
+    Console.Write($"Input array: ");
+    for (int i = 0; i < InputArray.Length; i++) Console.Write($"{InputArray[i]} ");
+    Console.WriteLine();
+}
+
+void MethodCreateOutputArray(string[] OutputArray)
+{
+    Console.Write($"Output array: ");
+    string[] newarr = new string[OutputArray.Length];
+    for (int i = 0; i < OutputArray.Length; i++)
+    {
+        if (OutputArray[i].Length <= 3)
+        {
+            newarr[i] = OutputArray[i];
+            Console.Write($"{newarr[i]} ");
+        }
+    }
+    Console.WriteLine();
+}
+
+string[] array = { "hello", "2", "world", ":-)",  "2", ":-)", "1234", "1567", "-2", "computer science", "-2", "Russia", "Denmark", "Kazan"};
+
+Console.WriteLine();
+MethodCreateInputArray(array);
+Console.WriteLine();
+MethodCreateOutputArray(array);
+Console.WriteLine();
